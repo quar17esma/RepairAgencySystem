080300c1086cc9f5d87b72836aa1b88371ba930e
quar17esma <quar17esma@gmail.com>
2018-08-21 16:26:31 +0300

    Added User entity

---
+++ src/main/java/com/quar17esma/entity/User.java
@@ -0,0 +1,163 @@
+package com.quar17esma.entity;
+
+import com.quar17esma.enums.Role;
+
+import java.time.LocalDate;
+
+public class User {
+    private int id;
+    private String email;
+    private String phone;
+    private String password;
+    private Role role;
+    private String name;
+    private String surname;
+    private LocalDate birthDate;
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", role=" + role +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
+    }
+
+    public static class Builder {
+        private  User user;
+
+        public Builder() {
+            this.user = new User();
+        }
+
+        public User build() {
+            return user;
+        }
+
+        public Builder setId(int id) {
+            user.setId(id);
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            user.setEmail(email);
+            return this;
+        }
+
+        public Builder setPhone(String phone) {
+            user.setPhone(phone);
+            return this;
+        }
+
+        public Builder setPassword(String password) {
+            user.setPassword(password);
+            return this;
+        }
+
+        public Builder setRole(Role role) {
+            user.setRole(role);
+            return this;
+        }
+
+        public Builder setName(String name) {
+            user.setName(name);
+            return this;
+        }
+
+        public Builder setSurname(String surname) {
+            user.setSurname(surname);
+            return this;
+        }
+
+        public Builder setBirthDate(LocalDate birthDate) {
+            user.setBirthDate(birthDate);
+            return this;
+        }
+    }
+}
