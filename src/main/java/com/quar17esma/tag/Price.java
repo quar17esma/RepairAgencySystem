6f04994c87da7fc84b0444c0ee4448835587cd70
quar17esma <quar17esma@gmail.com>
2018-08-23 01:46:05 +0300

    Added tag to show price

---
+++ src/main/java/com/quar17esma/tag/Price.java
@@ -0,0 +1,27 @@
+package com.quar17esma.tag;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.TagSupport;
+import java.io.IOException;
+
+public class Price extends TagSupport {
+    private int price;
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public int doStartTag() throws JspException {
+        try {
+            String priceString = String.valueOf(price);
+            String integerPart = priceString.substring(0, priceString.length() - 2);
+            String fractionalPart = priceString.substring(priceString.length() - 2);
+
+            pageContext.getOut().write(integerPart + "," + fractionalPart);
+        } catch (IOException e) {
+            throw new JspException(e.getMessage());
+        }
+        return SKIP_BODY;
+    }
+}
