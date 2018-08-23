da42f0df492fa19ad76804ab44ea8287917fc972
quar17esma <quar17esma@gmail.com>
2018-08-23 15:35:48 +0300

    Added login action

---
+++ src/main/java/com/quar17esma/controller/action/impl/Login.java
@@ -0,0 +1,53 @@
+package com.quar17esma.controller.action.impl;
+
+import com.quar17esma.controller.action.Action;
+import com.quar17esma.controller.manager.ConfigurationManager;
+import com.quar17esma.controller.manager.LabelManager;
+import com.quar17esma.entity.User;
+import com.quar17esma.exceptions.LoginException;
+import com.quar17esma.service.ILoginService;
+import com.quar17esma.service.impl.LoginService;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class Login implements Action {
+    private ILoginService loginService;
+
+    public Login() {
+        this.loginService = LoginService.getInstance();
+    }
+
+    public Login(ILoginService loginService) {
+        this.loginService = loginService;
+    }
+
+    @Override
+    public String execute(HttpServletRequest request) {
+        String page = null;
+
+        String locale = getLocaleOrSetDefault(request);
+
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        try {
+            User user = loginService.login(email, password);
+            request.getSession().setAttribute("user", user);
+            page = ConfigurationManager.getProperty("path.page.welcome");
+        } catch (LoginException e) {
+            request.setAttribute("errorLoginPassMessage",
+                    LabelManager.getProperty("message.login.error", locale));
+            page = ConfigurationManager.getProperty("path.page.login");
+        }
+
+        return page;
+    }
+
+    private String getLocaleOrSetDefault(HttpServletRequest request) {
+        String locale = (String) request.getSession().getAttribute("locale");
+        if (locale == null) {
+            request.getSession().setAttribute("locale", LabelManager.DEFAULT_LOCALE);
+        }
+        return locale;
+    }
+}
