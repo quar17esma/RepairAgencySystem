b6aa1cbd5f32abdbee4757d0e6224f8210e69536
quar17esma <quar17esma@gmail.com>
2018-09-04 16:05:26 +0300

    impl user checker test

--- src/main/java/com/quar17esma/controller/checker/InputUserChecker.java
+++ src/main/java/com/quar17esma/controller/checker/InputUserChecker.java
@@ -7,6 +7,7 @@ public class InputUserChecker extends InputDataChecker {
     private static final int EMAIL_LENGTH_MAX = 100;
     private static final int PHONE_LENGTH_MAX = 30;
     private static final int PASSWORD_LENGTH_MAX = 30;
+    private static final LocalDate BIRTH_DATE_MIN = LocalDate.of(1900, 1, 1);
 
     public boolean isInputDataCorrect(String name, String surname, String email, String phone, String password, LocalDate birthDate) {
         return (isNameCorrect(name) &&
@@ -17,12 +18,12 @@ public class InputUserChecker extends InputDataChecker {
                 isBirthDateCorrect(birthDate));
     }
 
-    private boolean isNameCorrect(String name) {
+    public boolean isNameCorrect(String name) {
         return checkString(name, CheckPatterns.NAME_RU, NAME_LENGTH_MAX) ||
                 checkString(name, CheckPatterns.NAME_EN, NAME_LENGTH_MAX);
     }
 
-    private boolean isPasswordCorrect(String password) {
+    public boolean isPasswordCorrect(String password) {
         return checkString(password, CheckPatterns.PASSWORD, PASSWORD_LENGTH_MAX);
     }
 
@@ -30,11 +31,12 @@ public class InputUserChecker extends InputDataChecker {
         return checkString(phone, CheckPatterns.PHONE, PHONE_LENGTH_MAX);
     }
 
-    private boolean isEmailCorrect(String email) {
+    public boolean isEmailCorrect(String email) {
         return checkString(email, CheckPatterns.EMAIL, EMAIL_LENGTH_MAX);
     }
 
-    private boolean isBirthDateCorrect(LocalDate birthDate) {
-        return birthDate.isBefore(LocalDate.now());
+    public boolean isBirthDateCorrect(LocalDate birthDate) {
+        return birthDate.isBefore(LocalDate.now()) &&
+                birthDate.isAfter(BIRTH_DATE_MIN);
     }
 }
